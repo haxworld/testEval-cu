71d8a2b130babbf07d7e67d3d370f1e0c7587ee0
Naman Khare <hi@namankhare.com>
2022-03-21 13:42:13 +0530

    firebase auth

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const db = require('./config/mongoose');
+// const db = require('./config/mongoose');
 const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors')
+
+//firebase
+const { register, signIn } = require('./controller/auth');
+require('./firebase.service')
+
 
 app.use(cors())
 app.use(express.urlencoded());
@@ -24,21 +29,34 @@ app.get("/login", (req, res) => {
     res.render('sign_in');
 })
 
-
-
-
-
-
 app.get("/signup", (req, res) => {
-
+    res.render('sign_up');
 })
 
 app.post("/login", (req, res) => {
-
+    let data = req.body;
+    let email = data.email
+    let password = data.password
+    signIn(email, password)
+        .then((value) => {
+            return res.send(value)
+        })
+        .catch((err) => {
+            return res.send(err)
+        })
 })
 
 app.post("/signup", (req, res) => {
     let data = req.body;
+    let email = data.email
+    let password = data.password
+    register(email, password)
+        .then((value) => {
+            return res.send(value)
+        })
+        .catch((err) => {
+            return res.send(err)
+        })
 
 })
 
