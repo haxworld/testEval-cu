f2ca1f0980af52f5d34ef9a7f076b02be3a434a9
Naman Khare <hi@namankhare.com>
2022-04-04 01:33:31 +0530

    exam demo

--- routes/ProfileRoute.js
+++ routes/ProfileRoute.js
@@ -2,16 +2,22 @@ const express = require('express');
 const userModel = require('../models/userModel');
 const ProfileRoute = express.Router();
 
+// ProfileRoute
+//     .get('/profile', async (req, res) => {
+//         try {
+//             let user = await userModel.findOne({ _id: req.user.id })
+//             const { name, email, username, photo, collegeName, graduationYear, createdAt } = user
+//             return res.json({ name, email, username, photo, collegeName, graduationYear, joined: createdAt })
+//         } catch (error) {
+//             return res.json("redirect to login")
+//         }
+//     })
+
 ProfileRoute
-    .get('/profile', async (req, res) => {
-        try {
-            let user = await userModel.findOne({ _id: req.user.id })
-            const { name, email, username, photo, collegeName, graduationYear, createdAt } = user
-            return res.json({ name, email, username, photo, collegeName, graduationYear, joined: createdAt })
-        } catch (error) {
-            return res.json("redirect to login")
+    .get('/profile', (req, res) => {
+        const data = {
+            title: 'Profile',
         }
+        return res.render('admin/profile', { data });
     })
-
-
 module.exports = ProfileRoute
